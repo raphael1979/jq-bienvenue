3c4cb2dc4a2c5a5c68ba89497508f78d1dd6f1dc
raphael <moudens.raphael@orange.fr>
2017-01-06 11:46:25 +0100

    fonctionnel

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Récuperez les valeurs des champs du formulaire et en faire un joli objet qui sera affiché dans la console.
 
-Concaténez les champs firs_name et last_name et dites bonjour à notre cher visiteur dans le titre en replacant le contenu du span #username
+Concaténez les champs firs_name et last_name et dites bonjour à notre cher visiteur dans le titre en replacant le contenu du span #username  
--- app.js
+++ app.js
@@ -1,1 +1,38 @@
-console.log('have fun !');
+var joliObj = new Object();
+
+
+$("input").bind("keyup", function(x) {
+	if (x.which !== 13) {
+		id = $(this).attr("id");
+		input = $(this).val();
+
+	}
+	else if (x.which === 13){
+		$('input').each(function(){
+			tId = $(this).attr("id");
+			tInput = $(this).val();
+
+			if(tInput.length > 0){
+				joliObj[tId] = tInput;
+				console.log(input + " " + tInput);
+				console.log(joliObj);
+			}
+			else{
+				console.log("rien");
+			}
+		})
+		
+	}});
+
+$('button').click(function(){
+	$('#username').html(joliObj.first_name + " " + joliObj.last_name);
+	console.log(joliObj);
+})
+
+
+
+
+
+
+
+
